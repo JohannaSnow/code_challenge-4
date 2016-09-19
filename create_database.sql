153d045cdb612e62d8463fab76a77a6d413dcbd5
Johanna Snow <johannadsnow@gmail.com>
2016-09-19 16:24:36 -0500

    fixes

--- create_database.sql
+++ create_database.sql
@@ -1,3 +1,19 @@
 -- Database name
+codechallenge4
 
 -- Document you create tables pSQL here
+CREATE TABLE treats(
+	id SERIAL PRIMARY KEY,
+	treatname VARCHAR(140),
+	description VARCHAR(140),
+	pic Boolean
+);
+
+---test an item--
+INSERT INTO treats VALUES(1, 'cake', 'chocolate', 'Y');
+
+
+
+----- I just did a boolean for a pic as in whether or not the treat had one,
+--I did try a couple of things, but none of them worked for storing an img on
+--the table.
---
+++ server/app.js
@@ -0,0 +1,76 @@
+var express = require(' express ');
+var app = express();
+var path = require('path');
+var urlencodedParser = bodyParser.urlencoded({
+    extended: false
+});
+var pg = require('pg');
+var connectionString = 'postgress://localhost:5432/codechallenge4';
+var port = process.env.PORT || 3000;
+
+
+//static folder
+
+app.use(express.static('public'));
+
+//spin up server
+app.listen(port, function() {
+    console.log(' server up on', port);
+});
+
+//base url
+app.get('/', function(req, res) {
+    console.log('base url hit');
+    res.sendFile(path.resolve('public/index.html'));
+});
+
+
+app.get(' /getTreats', function(req, res) {
+    console.log('getTreatsroute hit');
+    //connect to db
+    pg.connect(connectionString, function(err, client, done) {
+        //if error
+        if (err) {
+            console.log(err);
+        } // end error connect
+        else {
+            console.log('connected to db');
+            //array to hold our results
+            var resultsArray = [];
+            //query to db
+            var queryResults = client.query('SELECT * FROM treat');
+            console.log(queryResults);
+            queryResults.on(' row', function(row) {
+              //push each row of db into results array
+                resultsArray.push(row);
+            }); // end on row
+            queryResults.on(' end', function() {
+              //let the db know we're done
+                done();
+                //send the response back to the client
+                res.send( resultsArray );
+            }); // end query results
+        } //end else
+    }); //end connect (PG)
+}); // end get treats route
+
+//add to treats a description and pic
+app.post('/addTreats', urlencodedParser, function(req, res) {
+    console.log('addTreats route hit:', req.body);
+    //create DB data
+    var treat = req.body.treat;
+    var pic = req.body.pic;
+    pg.connect(connectionString, function(err, client, done) {
+        //if err
+        if (err) {
+            console.log(err);
+        } // successful conncetions
+        else {
+            console.log("conncected to the db");
+            //make a client var
+            client.query("INSERT INTO treats) VAlUES($1, $2, $3);" [treatname, description, pic]);
+            console.log("treat added to DB");
+            done();
+        } //end else
+    }); //end connect (PG)
+}); // end add treat
